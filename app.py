6d4455bc4c218bdbbe334d120722fe4af00bb3ce
madhavimuthusamy823-sudo <madhavimuthusamy823@gmail.com>
2026-08-13 13:02:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-app=Flask(_name_)
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-return"<h1>Hellow from Cloud!</h1><p>Develoyed on Render</p>"
+    return "<h1>Hello from Cloud!</h1><p>Deployed on Render</p>"
+
 @app.route('/Status')
 def Status():
-return{
-"Status":"running",
-"server":"Render"
-}
-if_name_=="_main_":
-app.run(host="0.0.0.0",port=5000)
+    return {
+        "Status": "running",
+        "server": "Render"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
